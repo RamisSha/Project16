859beec0e63fc692171d9e7ab28f1e76edfd5c6a
RamisSha <ramiskhan6386@gmail.com>
2023-07-27 12:57:42 +0530

    first commit

---
+++ app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def data (request):
+    D={'Name':'Ramis Khan','Age': 22,'DOB': '29-3-2001'}
+    return render(request,'index.html',context=D)
+
+def string (request):
+    return HttpResponse ('<center> <h2> Login Sucessfull <h2> </center>')
+
