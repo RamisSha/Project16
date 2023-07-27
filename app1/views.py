88b2cbc1ccb090a3615bd0253e7c9be4ccd1e3aa
RamisSha <ramiskhan6386@gmail.com>
2023-07-27 13:06:47 +0530

    first commit

--- app1/views.py
+++ app1/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def data (request):
     D={'Name':'Ramis Khan','Age': 22,'DOB': '29-3-2001'}
-    return render(request,'index.html',context=D)
+    return render(request,'data.html',context=D)
 
 def string (request):
     return HttpResponse ('<center> <h2> Login Sucessfull <h2> </center>')
